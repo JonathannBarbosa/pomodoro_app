20be50e185461fbad982ca0630cbe3aea9c76e3e
Jonathan Barbosa <jonathanbazevedo@hotmail.com>
2025-11-21 22:41:22 -0300

    feat: iniciando estilização do drawer

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,9 +2,23 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
-    </GestureHandlerRootView>
-  );
-}w
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+              <Drawer
+                  screenOptions={{
+      headerStyle: {
+          backgroundColor: '#021123'
+      },
+      headerTintColor: '#FFF',
+      drawerStyle: {
+          backgroundColor: '#021123'
+      },
+      drawerLabelStyle: {
+          color: '#FFF'
+      }
+  }}
+            >
+            </Drawer>
+        </GestureHandlerRootView>
+    );
+}
---
+++ app/add-task/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function AddTask() {
+    return (
+        <View>
+            <Text>
+                Adicionar uma tarefa
+            </Text>
+        </View>
+    );
+}
